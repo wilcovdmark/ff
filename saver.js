45874ac37856b9ecc9fba038ebb37847b6e535e2
zadiacuza <31917344+zadiacuza@users.noreply.github.com>
2017-09-20 09:50:18 +0200

    Add files via upload

---
+++ saver.js
@@ -0,0 +1,51 @@
+var canvas = document.querySelector("canvas");
+var ctx = canvas.getContext("2d");
+
+var ctx = canvas.getContext('2d');
+
+setTimeout(function () {
+
+    document.querySelector(".next").addEventListener("click", function () {
+        html2canvas(document.querySelector("main"), {
+            canvas: canvas,
+            allowTaint: true
+        }).then(function (canvas) {
+            console.log('Drew on the existing canvas');
+        });
+    }, false);
+
+}, 2000);
+
+function download() {
+    var dt = canvas.toDataURL('image/png');
+    this.href = dt;
+};
+
+downloadLnk.addEventListener('click', download, false);
+
+(function () {
+    var canvas = document.getElementById('canvas'),
+        context = canvas.getContext('2d');
+
+    // resize the canvas to fill browser window dynamically
+    window.addEventListener('resize', resizeCanvas, false);
+
+    function resizeCanvas() {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+
+        /**
+         * Your drawings need to be inside this function otherwise they will be reset when 
+         * you resize the browser window and the canvas goes will be cleared.
+         */
+        drawStuff();
+    }
+    resizeCanvas();
+
+    function drawStuff() {
+        // do your drawing stuff here
+    }
+
+    setTimeout(explode, 2000);
+
+})();
