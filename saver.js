ea05f89691df03faf30f0c57b66289dddb4fdc1b
fkimpe <fkimpe@deltion.nl>
2017-09-20 13:09:37 +0200

    cleared up

--- saver.js
+++ saver.js
@@ -1,5 +1,18 @@
+//canvas opzetten
 var canvas = document.querySelector("canvas");
 var ctx = canvas.getContext("2d");
+
+//canvas schalen
+resizeCanvas();
+window.addEventListener('resize', resizeCanvas, false);
+
+function resizeCanvas() 
+{
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;        
+}
+
+
 
 setTimeout(function () {
 
@@ -12,36 +25,10 @@ setTimeout(function () {
 
 }, 2000);
 
-function download() {
+downloadLnk.addEventListener('click', download, false);
+
+function download(this) {
     var dt = canvas.toDataURL('image/png');
     this.href = dt;
 };
 
-downloadLnk.addEventListener('click', download, false);
-
-(function () {
-    var canvas = document.getElementById('canvas'),
-        context = canvas.getContext('2d');
-
-    // resize the canvas to fill browser window dynamically
-    window.addEventListener('resize', resizeCanvas, false);
-
-    function resizeCanvas() {
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
-
-        /**
-         * Your drawings need to be inside this function otherwise they will be reset when 
-         * you resize the browser window and the canvas goes will be cleared.
-         */
-        drawStuff();
-    }
-    resizeCanvas();
-
-    function drawStuff() {
-        // do your drawing stuff here
-    }
-
-    setTimeout(explode, 2000);
-
-})();
